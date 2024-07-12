c41075743133f31a55112f208d4211924291dfa1
007rabbit <140362839+007rabbit@users.noreply.github.com>
2024-07-12 22:39:00 +0800

    Add files via upload

---
+++ Cnnp_dnet.py
@@ -0,0 +1,113 @@
+import torch
+import torch.nn as nn
+from torchsummary import summary
+import torch.nn.functional as F
+import cv2
+import numpy as np
+
+class Cnnp(nn.Module):
+    def __init__(self):
+        super(Cnnp, self).__init__()
+        self.p0 = nn.Sequential(
+            nn.Conv2d(in_channels=1, out_channels=64, kernel_size=5, stride=1, padding=2),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+        )
+        self.dw1 = nn.Sequential(
+            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=2, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+        )
+        self.dw2 = nn.Sequential(
+            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, stride=2, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+        )
+        self.p01 = nn.Sequential(
+            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+        )
+        # decoder
+        self.up2 = nn.Sequential(
+            CAA(256,1),
+            nn.ConvTranspose2d(in_channels=256, out_channels=128, kernel_size=4, stride=2, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+        )
+        self.up1 = nn.Sequential(
+            CAA(256, 1),
+            nn.ConvTranspose2d(in_channels=256, out_channels=128, kernel_size=4, stride=2, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+        )
+        self.up0 = nn.Sequential(
+            CAA(128+64, 1),
+            nn.Conv2d(in_channels=128+64, out_channels=96, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels=96, out_channels=64, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+        )
+        '''2个resnet模块'''
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=1),
+        )
+        self.relu1 = nn.ReLU(inplace=True)
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=1),
+        )
+        self.relu2 = nn.ReLU(inplace=True)
+
+        '''输出'''
+        self.p3 = nn.Sequential(
+            nn.Conv2d(in_channels=64, out_channels=32, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels=32, out_channels=1, kernel_size=3, stride=1, padding=1),
+        )
+    def forward(self, image):
+        p0 = self.p0(image)
+        dw1 = self.dw1(p0)
+        dw2 = self.dw2(dw1)
+        p1 = self.p01(dw2)
+        up2 = self.up2(torch.cat((p1, dw2), dim=1))
+        up1 = self.up1(torch.cat((up2, dw1), dim=1))
+        up0 = self.up0(torch.cat((up1, p0), dim=1))
+        '''resnet模块'''
+        out1 = self.conv1(up0)
+        out2 = self.relu1(out1+up0)
+        out3 = self.conv2(out2)
+        out4 = self.relu2(out3+out2)
+        p3 = self.p3(out4)
+        return p3
+
+class CAA(nn.Module):
+    def __init__(self,inc,rat):
+        super(CAA, self).__init__()
+        self.gap = nn.AdaptiveAvgPool2d((1, 1))
+        self.f1 = nn.Conv2d(inc,inc*rat,kernel_size=1)
+        self.f2 = nn.Conv2d(inc, inc * rat, kernel_size=1)
+        self.g = nn.Conv2d(inc*rat, inc, kernel_size=1)
+        self.relu = nn.ReLU(inplace=True)
+        self.sig = nn.Sigmoid()
+    def forward(self,x):
+        x0=x
+        x1 = self.gap(x)
+        f1=self.f1(x1)
+        f2=self.f2(x1)
+        x=self.relu(f1)*f2
+        x=self.sig(self.g(x))
+        return x*x0
+
+
+
+
---
+++ cnnp_train.py
@@ -0,0 +1,162 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import Cnnp_dnet
+import numpy as np
+from torchvision import datasets, transforms
+import argparse
+import torch.utils.data
+import torch.nn.functional as F  # 小写变量导入为非小写
+import time
+import random
+'''0,255像素范围内训练'''
+
+parser = argparse.ArgumentParser(description='train UCANet')
+parser.add_argument('--batch-size', type=int, default=8, metavar='N',
+                    help='input batch size for trainning')
+parser.add_argument('--epochs', type=int, default=1500, metavar='N',
+                    help='number of epochs to train ')
+parser.add_argument('--lr', type=float, default=0.0001, metavar='LR',
+                    help='learning rate of train')
+parser.add_argument('--weight_decay', type=float, default=0.001, metavar='wd',
+                    help='weight_deacy')
+parser.add_argument('--betas', type=tuple, default=(0.9, 0.999),
+                    help='Adam BETA paramters.')
+parser.add_argument('--no-cuda', action='store_true', default=False,
+                    help='disables CUDA training')
+parser.add_argument('--seed', type=int, default=1, metavar='S',
+                    help='random seed (default: 1)')
+parser.add_argument('--log-interval', type=int, default=10, metavar='N',
+                    help='how many batches to wait before logging training status')
+args = parser.parse_args()
+args.cuda = not args.no_cuda and torch.cuda.is_available()  # 判断GPU是否可用-----is_available
+torch.manual_seed(args.seed)  # 设置cpu随机种子,生成随机数
+if args.cuda:
+    torch.cuda.manual_seed(args.seed)  # 设置gpu随机种子
+else:
+    args.gpu = None
+kwargs = {'num_workers': 0, 'pin_memory': True} if args.cuda else {}  # pin_memory拷贝数据到GPU,num_workers设置工作进程
+def custom_rotation(img):
+    # 生成90度的倍数的旋转角度
+    angle = random.randint(0, 3) * 90
+    return transforms.functional.rotate(img, angle)
+'''训练过程'''
+train_path2 = r"D:\haiyang\datasets\BOWS_512_512_3000"  # 设置路径
+train_data = datasets.ImageFolder(train_path2, transform=transforms.Compose([
+    transforms.Grayscale(),
+    transforms.Lambda(custom_rotation),
+    transforms.ToTensor(),  # transforms.ToTensor()将像素值从整数范围[0, 255]转换为浮点数范围[0, 1]
+]))
+
+train_loader = torch.utils.data.DataLoader(train_data, batch_size=args.batch_size, shuffle=True, **kwargs)
+'''验证集'''
+valid_path2 = r"D:\haiyang\datasets\ceshitu"  # 设置路径
+valid_data = datasets.ImageFolder(valid_path2, transform=transforms.Compose([
+    transforms.Grayscale(),
+    transforms.Resize((512,512)),
+    transforms.ToTensor()
+]))
+valid_loader = torch.utils.data.DataLoader(valid_data, batch_size=args.batch_size, shuffle=True, **kwargs)
+model = Cnnp_dnet.Cnnp()
+if args.cuda:
+    model.cuda()
+optimer = optim.AdamW(model.parameters(), lr=args.lr, betas=args.betas, eps=1e-08,
+                     weight_decay=args.weight_decay, amsgrad=False)  # 选用Adam优化器
+
+
+def pre_img(img,mask, flag):
+    device = img.device
+    img1 = img.clone().to(device)
+    '''创建索引分量'''
+    I1 = torch.zeros(img.shape, device=device)
+    I2 = torch.zeros(img.shape, device=device)
+    I1[:, :, :, :] = img1[:, :, :, :]*(mask == flag)
+    I2[:, :, :, :] = img1[:, :, :, :]*(mask != flag)
+    return I1, I2
+
+
+def result_deal(img,mask, flag):
+    device = img.device
+    img1 = img.clone().to(device)
+    I1 = torch.zeros(img.shape, device=device)
+    I1[:, :, :, :] = img1[:, :, :, :] * (mask == flag)
+    return I1
+
+
+def train(epoch):
+    lr_train = (optimer.state_dict()['param_groups'][0]['lr'])
+    print('lr_train=', lr_train)
+    model.train()  # 激活训练模型
+    '''使用卷积核进行预处理'''
+    for idx, (data, lable) in enumerate(train_loader):
+        flag = random.randint(1, 4)
+        x_, y_ = np.random.randint(0, 384, size=2)
+        data = ((data*255)[:, :, x_:x_ + 128, y_:y_ + 128]).cuda()
+        mask = category[x_:x_ + 128, y_:y_ + 128].cuda()
+        i1, img = pre_img(data,mask, flag)
+        img.requires_grad = True  # 张量自动求梯度，替换掉Variable()用法
+        data2 = model(img)
+        pre_ = result_deal(data2,mask, flag)  # 返回I1的预测值
+        loss = F.mse_loss(pre_, i1)
+        optimer.zero_grad()  # 全局梯度为0
+        loss.backward()
+        optimer.step()
+        '''反向传播'''
+        if (idx + 1) % 50 == 0:
+            print('Train Epoch: {}   [{}/{} ({:.0f}%)], loss={}'.format(
+                epoch, (idx + 1) * len(data), len(train_loader.dataset),
+                       100. * (idx + 1) / len(train_loader), loss))
+    return loss.item()
+
+def valid():
+    model.eval()
+    loss0 = 0
+    for idx, (data, label) in enumerate(valid_loader):
+        data = (data*255).cuda()
+        flag = random.randint(1, 4)
+        """0点1叉"""
+        with torch.no_grad():
+            i1, img = pre_img(data,category, flag)
+            data2 = model(img)
+            pre_ = result_deal(data2,category, flag)  # 返回I1的预测值
+            loss = F.mse_loss(pre_, i1)
+        loss0 += loss.item()
+    print("avg-valid-loss=", loss0/(idx+1))
+    return loss0/(idx+1)
+
+category = torch.zeros((512,512), dtype=torch.long, device='cuda')
+category[::2, ::2] = 1
+category[::2, 1::2] = 2
+category[1::2, ::2] = 3
+category[1::2, 1::2] = 4
+import datetime
+import pandas as pd
+if __name__ == '__main__':
+    current_datetime = datetime.datetime.now()
+    formatted_datetime = current_datetime.strftime("%Y-%m-%d %H:%M:%S")
+    print('程序启动******', "日期和时间：", formatted_datetime)
+    t3 = time.time()
+    lo = []
+    print('当前学习率',optimer.param_groups[0]['lr'])
+    for epoch in range(args.epochs):
+        if epoch%400==0 :optimer.param_groups[0]['lr'] = optimer.param_groups[0]['lr']*0.5
+        t1 = time.time()
+        loss1 = train(epoch)
+        t2=time.time()
+        loss2 = valid()
+        lo.append({'epoch':epoch,'train-loss':loss1,'test-loss':loss2})
+        df = pd.DataFrame(lo)
+        df.to_excel('train-loss.xlsx', index=False)
+        '''loss保存，参数保存'''
+        if epoch>400:
+            state = {'network': model.state_dict(), 'optimizer_learn_rate': optimer.param_groups[0]['lr']}
+            torch.save(state, './train_param/' + str(epoch) + '_model.pth')
+        t2 = time.time()
+        print('total time = ', (t2-t1)//60, '分钟', (t2-t1) % 60, '秒')
+    t4 = time.time()
+    print('total time = ', (t4-t3)//3600, '小时', ((t4-t3) % 3600)//60, '分钟', (t4-t3) % 60, '秒')
+    current_datetime = datetime.datetime.now()
+    formatted_datetime = current_datetime.strftime("%Y-%m-%d %H:%M:%S")
+    print("当前日期和时间：", formatted_datetime)
+
+
